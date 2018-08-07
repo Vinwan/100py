7d3c44661db9614de029b3cea046a697bdc920fa
Vinwan <wellber.wan@gmail.com>
2018-08-07 21:47:04 +0800

    add Pool

--- spiderman/1.4.1.py
+++ spiderman/1.4.1.py
@@ -1,30 +1,17 @@
-#coding:utf-8
+from multiprocessing import Pool
+import os, time, random
 
-import os
-from multiprocessing import Process
+def run_task(name):
+    print('Task %s (pid = %s) is running... ' % (name, os.getpid()))
+    time.sleep(random.random() * 3)
+    print('Task %s end.' % name)
 
-'''
-# 使用 os 模块中的 fork 方式实现多进程
-if __name__ == '__main__':
-    print('current Process (%s) start ...' % (os.getpid()))
-    pid = os.fork()
-    if pid < 0:
-        print('error in fork')
-    elif pid == 0:
-        print('I am child process(%s) and my parent process is (%s)' % (os.getpid(), os.getppid()))
-    else:
-        # if [,] instead [%], the result number display in the end.
-        print('I(%s) created a child process (%s).', (os.getppid(), pid))
-'''
-
-#  使用 multiprocessing 模块创建多进程
-def run_proc(name):
-    print('Child process %s (%s) Running...' % (name, os.getpid()))
-if __name__ == '__main__':
-    print('Parent process %s.' % os.getpid())
+if __name__=='__main__':
+    print('Current process %s.' % os.getpid())
+    p = Pool(processes=3)
     for i in range(5):
-        p = Process(target=run_proc, args=(str(i),))
-        print('Process will start.')
-        p.start()
+        p.apply_async(run_task, args=(i,))
+    print('Waiting for all subprocesses done... ')
+    p.close()
     p.join()
-    print('Process end.')
+    print('All subprocesses done.')
